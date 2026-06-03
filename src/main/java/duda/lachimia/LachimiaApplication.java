edda383d43764e871465539f2edc01c988fcfa2c
unknown <madudstan@gmail.com>
2026-06-03 19:52:42 -0300

    primeiro commit

---
+++ src/main/java/duda/lachimia/LachimiaApplication.java
@@ -0,0 +1,13 @@
+package duda.lachimia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LachimiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LachimiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duda/lachimia/LachimiaApplicationTests.java
@@ -0,0 +1,13 @@
+package duda.lachimia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LachimiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
